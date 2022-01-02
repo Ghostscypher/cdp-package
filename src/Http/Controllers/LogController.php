bb2435111d03556e1d55a566c5794db2fe5a7efc
Ghostscypher <bngetich69@gmail.com>
2022-01-02 16:22:58 +0300

    checkpoint 49

--- src/Http/Controllers/LogController.php
+++ src/Http/Controllers/LogController.php
@@ -10,7 +10,8 @@ class LogController
 {
    public function getLogs(Request $request, $type = null)
    {
-        return new ApiResource(app('cdp_credentials')
+        return new ApiResource(
+            app('cdp_credentials')
             ->service->logs()
             ->when($type !== '', function($query) use ($type) {
                 return $query->where('type', $type);
@@ -21,7 +22,7 @@ class LogController
    public function getServiceLogs($service_uuid, $type)
    {
         return new ApiResource(
-                CDP::serviceClass()
+                CDP::serviceModel()
                 ->where('service_uuid', $service_uuid)
                 ->service->logs()
                 ->when($type !== '', function($query) use ($type) {
