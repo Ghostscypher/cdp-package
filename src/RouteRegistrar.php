a40f7630276328119de0067427310d2f691e3cae
Ghostscypher <bngetich69@gmail.com>
2022-01-02 16:38:46 +0300

    checkpoint 53

--- src/RouteRegistrar.php
+++ src/RouteRegistrar.php
@@ -54,8 +54,8 @@ class RouteRegistrar
                 $router->get('logs/{type?}', [LogController::class, 'getLogs'])->name('cdp.logs.index');
                 $router->get('logs/{service_uuid}/{type?}', [LogController::class, 'getServiceLogs'])->name('cdp.log.type.index');
 
-                $router->get('logs/{name}/{type?}', [LogController::class, 'getLogsByName'])->name('cdp.logs.name.index');
-                $router->get('logs/{service_uuid}/{name}/{type?}', [LogController::class, 'getServiceLogsByName'])->name('cdp.log.name.index');
+                $router->get('logs/name/{name}/{type?}', [LogController::class, 'getLogsByName'])->name('cdp.logs.name.index');
+                $router->get('logs/name/{service_uuid}/{name}/{type?}', [LogController::class, 'getServiceLogsByName'])->name('cdp.log.name.index');
             });
     }
 
